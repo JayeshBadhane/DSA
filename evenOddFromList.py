4fdfafbbff6161cbc5470f5d7310e7d52143d9a6
Jayesh Bhadane <134684685+JayeshBadhane@users.noreply.github.com>
2024-05-02 19:38:31 +0530

    Update evenOddFromList.py

--- evenOddFromList.py
+++ evenOddFromList.py
@@ -1,11 +1,9 @@
-N,X=map(int,input().split())
+N=map(int,input().split())
 li=[N]
 li=map(int,input().split())
-
-if X in li:
+for i in li:
     if X/2==0:
         print("Even Number")
     else:
         print("Odd Number")
-else:
-    print("Number is'nt exist in list")
+    
